d9ba6da06010bb311cb12d8c6bbb9b02d5a9ea41
hermesonbastos <hermesonbastos@gmail.com>
2024-01-22 14:26:00 -0300

    feat: login routes

---
+++ src/Components/Login/LoginForm.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+    }).then(response => {
+      console.log(response);
+      return response.json();
+    }).then(json => {
+      console.log(json);
+    });
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;

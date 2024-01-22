b00479d28ec589980036b25e16f41ea411492e19
hermesonbastos <hermesonbastos@gmail.com>
2024-01-22 13:00:30 -0300

    feat: header and main navigation

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import Dogs from "../Assets/dogs.svg?react";
 
 const Header = () => {
-  return <div className={styles.header}>
-    <nav className="container">
-      <Link to='/'>Home</Link>
-      <Link to='/login'>Login / Criar</Link>
-    </nav>
-  </div>;
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;

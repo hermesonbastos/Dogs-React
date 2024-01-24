982241f5b2853270433ac60d3d24a6feba83c595
hermesonbastos <hermesonbastos@gmail.com>
2024-01-24 01:13:39 -0300

    feat: add UserContext and refactor login funcionality

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,41 +3,18 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
-import { TOKEN_POST, USER_GET } from "../../api";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if(token) {
-      getUser(token);
-    }
-  }, []);
-
-  const { url, options } = TOKEN_POST({
-    username: username.value,
-    password: password.value,
-  });
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
+  const { userLogin } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const response = await fetch(url, options)
-      const json = await response.json();
-
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token);
-      console.log(json);
+      userLogin(username.value, password.value)
     }
   }
 

9d7c529ce9d8b0ea70c3bdd268d5d74739599e42
hermesonbastos <hermesonbastos@gmail.com>
2024-01-24 00:32:08 -0300

    feat: add token authentication and user retrieval

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,29 +3,41 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../api";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(event) {
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if(token) {
+      getUser(token);
+    }
+  }, []);
+
+  const { url, options } = TOKEN_POST({
+    username: username.value,
+    password: password.value,
+  });
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-        });
+      const response = await fetch(url, options)
+      const json = await response.json();
+
+      window.localStorage.setItem('token', json.token);
+      getUser(json.token);
+      console.log(json);
     }
   }
 

40532bb4c6513ad13c62c81706e2dd33766d94a7
hermesonbastos <hermesonbastos@gmail.com>
2024-02-06 21:02:57 -0300

    feat: load animation

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FeedPhotosItem from "./FeedPhotosItem";
 import useFetch from "../../Hooks/useFetch";
 import Error from "../Helper/Error";
-import Loading from "../Helper/Error";
+import Loading from "../Helper/Loading";
 import { PHOTOS_GET } from "../../api";
 import styles from "./FeedPhotos.module.css";
 
@@ -13,12 +13,9 @@ const FeedPhotos = ({ setModalPhoto }) => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { json } = await request(url, options);
-      // console.log(json);
     }
     fetchPhotos();
   }, [request]);
-
-  // console.log(data);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;

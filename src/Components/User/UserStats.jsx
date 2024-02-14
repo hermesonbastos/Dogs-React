7f3056aa73be0111922d5d39fcc28ff9257eba43
hermesonbastos <hermesonbastos@gmail.com>
2024-02-14 15:23:29 -0300

    feat: add stats graphs

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Head from '../Helper/Head'
+import useFetch from '../../Hooks/useFetch'
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
+  const {data, error, loading, request} = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request])
+
+  if(loading) return <Loading />
+  if(error) return <Error />
+  if(data)
   return (
     <div>
       <Head title="Estatísticas" />
-      Estatísticas
+      <UserStatsGraphs data={data} />
     </div>
   )
+  else return null;
 }
 
 export default UserStats
